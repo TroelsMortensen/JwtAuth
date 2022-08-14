87f26dac44ce0904ebaa979ac6126fe26d9f157a
TRMO <tromortensen@hotmail.com>
2022-08-14 12:53:42 +0200

    Initial commit

---
+++ WebApi/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models;
+
+namespace WebApi.Services;
+
+public interface IUserService
+{
+    Task<User> GetUser(string username, string password);
+    Task RegisterUser(User user);
+}
